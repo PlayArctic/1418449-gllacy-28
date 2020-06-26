0088fa03c576692e95c18e9f3e8b7af4863761c0
PlayArctic <65240326+PlayArctic@users.noreply.github.com>
2020-06-26 10:29:42 +0300

    JS + PixelPerfect

--- js/script.js
+++ js/script.js
@@ -9,8 +9,10 @@ var searchpopup = document.querySelector(".search-field");
 
 var feedback = document.querySelector(".feedback-btn");
 var feedbackpopup = document.querySelector(".feedback-form");
+var buttonclose = document.querySelector(".button-close");
 
 var cart = document.querySelector(".cart");
+var cartfull = document.querySelector(".cart-full");
 var cartpopup = document.querySelector(".cart-field");
 
 
@@ -40,33 +42,51 @@ if (feedback) {
 }
 
 
-if (cartpopup) {
+if (cartpopup && cart) {
        cart.addEventListener("click", function (evt) {
               evt.preventDefault();
               cartpopup.classList.add("visually-show-inlblock");
               modalwrapper.classList.add("visually-show");
+       });
+}
 
+if (cartpopup && cartfull) {
+       cartfull.addEventListener("click", function (evt) {
+              evt.preventDefault();
+              cartpopup.classList.add("visually-show-inlblock");
+              modalwrapper.classList.add("visually-show");
+       });
+}
+
+
+if (buttonclose) {
+       buttonclose.addEventListener("click", function (evt) {
+              evt.preventDefault();
+              modaldarkwrapper.classList.remove("visually-show");
+              feedbackpopup.classList.remove("visually-show");
        });
 }
 
 
 
-       modalwrapper.addEventListener("click", function () {
-              loginpopup.classList.remove("visually-show");
-              searchpopup.classList.remove("visually-show");
-              if (feedbackpopup) {
-                     feedbackpopup.classList.remove("visually-show");
-              }
-              if (cartpopup) {
-                     cartpopup.classList.remove("visually-show-inlblock");
-              }
-              modalwrapper.classList.remove("visually-show");
-       });
 
 
-       if (modaldarkwrapper) {
-              modaldarkwrapper.addEventListener("click", function () {
-                     feedbackpopup.classList.remove("visually-show");
-                     modaldarkwrapper.classList.remove("visually-show");
-              });
+modalwrapper.addEventListener("click", function () {
+       loginpopup.classList.remove("visually-show");
+       searchpopup.classList.remove("visually-show");
+       if (feedbackpopup) {
+              feedbackpopup.classList.remove("visually-show");
        }
+       if (cartpopup) {
+              cartpopup.classList.remove("visually-show-inlblock");
+       }
+       modalwrapper.classList.remove("visually-show");
+});
+
+
+if (modaldarkwrapper) {
+       modaldarkwrapper.addEventListener("click", function () {
+              feedbackpopup.classList.remove("visually-show");
+              modaldarkwrapper.classList.remove("visually-show");
+       });
+}
