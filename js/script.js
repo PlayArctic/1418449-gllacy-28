050f02d6b0a8743f94abfe08a5d66c2b50421977
PlayArctic <65240326+PlayArctic@users.noreply.github.com>
2020-07-11 20:29:59 +0300

    Исправления после проверки 11.07.20

--- js/script.js
+++ js/script.js
@@ -112,11 +112,11 @@ document.addEventListener('keydown', function (evt) {
 // Form feedback//
 
 var feedbackForm = document.querySelector('.feedback-form');
-if(feedbackForm) {
-var feedbackFormForm = feedbackForm.querySelector('form');
-var feedbackName = feedbackForm.querySelector('.customer-name');
-var feedbackEmail = feedbackForm.querySelector('.customer-email');
-var feedbackMessage = feedbackForm.querySelector('.customer-message');
+if (feedbackForm) {
+       var feedbackFormForm = feedbackForm.querySelector('form');
+       var feedbackName = feedbackForm.querySelector('.customer-name');
+       var feedbackEmail = feedbackForm.querySelector('.customer-email');
+       var feedbackMessage = feedbackForm.querySelector('.customer-message');
 }
 
 
@@ -193,44 +193,45 @@ if (bullet1) {
 // Яндекс карты// 
 //Функция ymaps.ready() будет вызвана, когда
 // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-
-ymaps.ready(function () {
-       var myMap = new ymaps.Map('map', {
-              center: [59.939880, 30.330689],
-              zoom: 15.5,
-              controls: [] //убираем слои, пробки и т.д.
-       }, {
-              //searchControlProvider: 'yandex#search'
-       }, {
-              suppressMapOpenBlock: true //убираем открыть в яндекс картах
-       }, {
-              behaviors: [] //линейка это поведение. убираем линейку
-       });
-
-       // Создаём макет содержимого.
-       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-              '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-       ),
-              myPlacemark = new ymaps.Placemark([59.938525, 30.323026], {
+if (typeof(ymaps) !== 'undefined') {
+       ymaps.ready(function () {
+              var myMap = new ymaps.Map('map', {
+                     center: [59.939880, 30.330689],
+                     zoom: 15.5,
+                     controls: [] //убираем слои, пробки и т.д.
               }, {
-                     // Опции.
-                     // Необходимо указать данный тип макета.
-                     iconLayout: 'default#image',
-                     // Своё изображение иконки метки.
-                     iconImageHref: 'img/pin_ice.svg',
-                     // Размеры метки.
-                     iconImageSize: [80, 140],
-                     // Смещение левого верхнего угла иконки относительно
-                     // её 'ножки' (точки привязки).
-                     iconImageOffset: [-40, -140],
-                     // Смещение слоя с содержимым относительно слоя с картинкой.
-                     iconContentOffset: [15, 15],
-                     // Макет содержимого.
-                     iconContentLayout: MyIconContentLayout
-              }),
+                     //searchControlProvider: 'yandex#search'
+              }, {
+                     suppressMapOpenBlock: true //убираем открыть в яндекс картах
+              }, {
+                     behaviors: [] //линейка это поведение. убираем линейку
+              });
+
+              // Создаём макет содержимого.
+              MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                     '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+              ),
+                     myPlacemark = new ymaps.Placemark([59.938525, 30.323026], {
+                     }, {
+                            // Опции.
+                            // Необходимо указать данный тип макета.
+                            iconLayout: 'default#image',
+                            // Своё изображение иконки метки.
+                            iconImageHref: 'img/pin_ice.svg',
+                            // Размеры метки.
+                            iconImageSize: [80, 140],
+                            // Смещение левого верхнего угла иконки относительно
+                            // её 'ножки' (точки привязки).
+                            iconImageOffset: [-40, -140],
+                            // Смещение слоя с содержимым относительно слоя с картинкой.
+                            iconContentOffset: [15, 15],
+                            // Макет содержимого.
+                            iconContentLayout: MyIconContentLayout
+                     }),
 
 
 
-              myMap.geoObjects
-                     .add(myPlacemark);
-});
+                     myMap.geoObjects
+                            .add(myPlacemark);
+       });
+}
